4a056e98f79c4cafc90ddb9f250dc1bdd270e421
flpvsk <filipovskii.off@gmail.com>
2023-11-18 14:54:58 +0100

    Wip

---
+++ _data/metadata.cjs
@@ -0,0 +1,5 @@
+module.exports = {
+  title: "Gnd.Buzz",
+  description: "",
+  language: "en",
+};
---
+++ eleventy.config.cjs
@@ -0,0 +1,24 @@
+const pluginWebc = require("@11ty/eleventy-plugin-webc");
+const { eleventyImagePlugin } = require("@11ty/eleventy-img");
+
+module.exports = function (eleventyConfig) {
+  eleventyConfig.ignores.add("./README.md");
+
+  eleventyConfig.addPlugin(pluginWebc, {
+    components: [
+      "./_components/**/*.webc",
+      "npm:@11ty/eleventy-img/*.webc",
+      "npm:@11ty/is-land/*.webc",
+    ],
+  });
+
+  eleventyConfig.addPlugin(eleventyImagePlugin, {
+    formats: ["webp", "jpeg", "png"],
+    urlPath: "/img/",
+
+    defaultAttributes: {
+      loading: "lazy",
+      decoding: "async",
+    },
+  });
+};
